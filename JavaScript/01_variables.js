7ee24a64a72b416166efc6df0cdf7263fe5a74bb
Saloni Kori <saloni@example.com>
2026-06-13 17:26:41 +0530

    Initial commit

---
+++ JavaScript/01_variables.js
@@ -0,0 +1,16 @@
+const accountNo = 4675876
+let accountName = "Saloni"
+var accountId = "7898099"
+accountCity = "Mumbai"
+let accountState;
+
+accountName="ABC"
+accountId="98908"
+accountCity="Delhi"
+
+// let and var are redeclarable but const cannot be redeclared
+// var is mostly not prefered because of issues in block scope and function scope
+
+console.log(accountNo);
+
+console.table([accountNo,accountName,accountId,accountCity,accountState])
---
+++ JavaScript/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+5) //Using node JS not browser
+
+console.log(3+3)
+console.log("Saloni")
+
+let name = "Zoha"
+let age = 18
+let state = false
+//number => 2 to power of 53
+//bigint => used for big numbers but mostly not used
+//string => ""
+//bolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null)   //object
+console.log(typeof undefined)   //undefined
