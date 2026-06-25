bd696a6aef255e837544a6c33990a284712cb9f4
Saloni Kori <saloni@example.com>
2026-06-25 20:49:42 +0530

    numbers and maths

--- JavaScript/06_nums_and_maths.js
+++ JavaScript/06_nums_and_maths.js
@@ -1,29 +1,29 @@
-// const score = 100
-// console.log(score);
+const score = 100
+console.log(score);
 
-// const balance = new Number(500)
-// console.log(balance);
+const balance = new Number(500)
+console.log(balance);
 
-// console.log(balance.toString());
-// console.log(balance.toString().length);    //after converting a number to a string we can also use string functions
-// console.log(balance.toFixed(2));       //after decimal point only 2 digits should be shown
+console.log(balance.toString());
+console.log(balance.toString().length);    //after converting a number to a string we can also use string functions
+console.log(balance.toFixed(2));       //after decimal point only 2 digits should be shown
 
-// const anotherNumber = 129.76
-// console.log(anotherNumber.toPrecision(3));     //130
-// console.log(anotherNumber.toPrecision(4));     //129.8
+const anotherNumber = 129.76
+console.log(anotherNumber.toPrecision(3));     //130
+console.log(anotherNumber.toPrecision(4));     //129.8
 
-// const hundreds = 100000000
-// console.log(hundreds.toLocaleString("en-IN"));     //adds comma based on region which is currenty set to INDIA but default it is USA
+const hundreds = 100000000
+console.log(hundreds.toLocaleString("en-IN"));     //adds comma based on region which is currenty set to INDIA but default it is USA
 
 
 //++++++++++++++++ MATHS +++++++++++++++++
-// console.log(Math);
-// console.log(Math.abs(-9));       //9
-// console.log(Math.round(5.8));    //6
-// console.log(Math.floor(4.6));    //4
-// console.log(Math.ceil(4.6));     //5
-// console.log(Math.min(3,5,81,8,9));
-// console.log(Math.max(3,5,81,8,9));
+console.log(Math);
+console.log(Math.abs(-9));       //9
+console.log(Math.round(5.8));    //6
+console.log(Math.floor(4.6));    //4
+console.log(Math.ceil(4.6));     //5
+console.log(Math.min(3,5,81,8,9));
+console.log(Math.max(3,5,81,8,9));
 
 console.log(Math.random());     //gives random values between 1 and 0
 console.log((Math.random()*10) + 1);     //gives random values between 1 and 10
